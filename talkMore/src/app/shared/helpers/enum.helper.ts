fdbda1b88d06b8cd312925d9b558f663f9155f7f
Bruno Braganca Mendes <bruno.mendes@linx.com.br>
2021-02-01 10:25:42 -0300

    removidos elementos não utilizados

--- talkMore/src/app/shared/helpers/enum.helper.ts
+++
@@ -1,5 +0,0 @@
-export function GetEnumKeyByEnumValue<T extends { [index: string]: any }>(myEnum: T, enumValue: string): keyof T | null {
-    let keys = Object.keys(myEnum).filter(x => myEnum[x] == enumValue);
-    return keys.length > 0 ? keys[0] : null;
-  }
-  
--- talkMore/src/app/shared/utils/enum.ts
+++
@@ -1,5 +0,0 @@
-export enum TalkMorePlan {
-	"FaleMais 30",
-    "FaleMais 60",
-    "FaleMais 120",
-}
